8ade9a4b7df71f24cc843e30fe8d2a8c8f844ca9
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-24 15:00:58 +0200

    Implement getting planets

--- server/src/server.js
+++ server/src/server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}...`);
 });
