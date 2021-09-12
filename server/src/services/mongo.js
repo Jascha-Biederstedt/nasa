506980c10c4e229c45e866750f3f47edacb5d5f3
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-09-12 13:13:36 +0200

    Add MongoDB for Github actions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_PW = require('../config');
-
-const MONGO_URL = `mongodb+srv://nasa-api:${MONGO_PW}@nasa.awiix.mongodb.net/nasa?retryWrites=true&w=majority`;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready...');
